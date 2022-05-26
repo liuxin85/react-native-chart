c3c6148317356b3905158581bb5b25c1ed002a3b
Mark Liu <liuxin85@gmail.com>
2022-05-26 19:53:21 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+import { VictoryChart, VictoryBar, VictoryTheme } from "victory-native";
+import data from "./data";
+
+const { width, height } = Dimensions.get("screen");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryBar
+          style={{ data: { fill: "orange", width: 15 } }}
+          barWidth={15}
+          width={width / 1.05}
+          animate
+          data={data}
+          x="month"
+          y="earnings"
+        />
+      </VictoryChart>
     </View>
   );
 }
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ data/index.ts
@@ -0,0 +1,27 @@
+const data = [
+  {
+    month: 1,
+    earnings: 541,
+  },
+  {
+    month: 2,
+    earnings: 570,
+  },
+  {
+    month: 3,
+    earnings: 465,
+  },
+  {
+    month: 4,
+    earnings: 700,
+  },
+  {
+    month: 5,
+    earnings: 641,
+  },
+  {
+    month: 6,
+    earnings: 381,
+  },
+];
+export default data;
